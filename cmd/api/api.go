45051323c38b4aa053b2435c7efbf92544a9c247
SaiAnish23 <anish231003@gmail.com>
2024-10-12 21:50:39 +0530

    feat:Router and Middleware added

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type application struct {
@@ -14,14 +17,28 @@ type config struct {
 	addr string
 }
 
-func (app *application) mount() *http.ServeMux {
-	mux := http.NewServeMux()
-	// mux.HandleFunc("/", app.home)
-	mux.HandleFunc("GET /v1/health", app.healthcheck)
-	return mux
+func (app *application) mount() *chi.Mux {
+	// mux := http.NewServeMux()
+	// // mux.HandleFunc("/", app.home)
+	// mux.HandleFunc("GET /v1/health", app.healthcheck)
+	// return mux
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	// r.Get("/v1/health", app.healthcheck)
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthcheck)
+	})
+	return r
+
 }
 
-func (app *application) run(mux *http.ServeMux) error {
+func (app *application) run(mux *chi.Mux) error {
 	// Start the server
 
 	// mux := http.NewServeMux()
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/SaiAnish23/Gophers
 
-go 1.22.5
+go 1.23
+
+require github.com/go-chi/chi/v5 v5.1.0
