50a8e6b3e758bff19713c8455ec70503237d7645
SaiAnish23 <anish231003@gmail.com>
2024-10-13 20:30:05 +0530

    feat:Added the env file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,21 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/SaiAnish23/Gophers/internal/env"
+	"github.com/joho/godotenv"
+)
 
 func main() {
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	cfg := config{
-		addr: ":4000",
+		addr: env.GetString("ADDR", ":4000"),
 	}
 
 	app := &application{
