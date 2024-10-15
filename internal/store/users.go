d12288febd5f0430da9ca9f8dba36d37f7e3239f
SaiAnish23 <anish231003@gmail.com>
2024-10-16 00:40:39 +0530

    feat: update store functions

--- internal/store/users.go
+++ internal/store/users.go
@@ -5,10 +5,37 @@ import (
 	"database/sql"
 )
 
-type UsersStore struct {
+type User struct {
+	ID        int64  `json:"id"`
+	Username  string `json:"username"`
+	Email     string `json:"email"`
+	CreatedAt string `json:"created_at"`
+	Password  string `json:"-"`
+}
+
+type UserStore struct {
 	db *sql.DB
 }
 
-func (s *UsersStore) Create(ctx context.Context) error {
+func (s *UserStore) Create(ctx context.Context, user *User) error {
+
+	query := `
+		INSERT INTO posts (content, title, user_id, tags)
+		VALUES ($1, $2, $3, $4) RETURNING id, created_at, updated_at
+	`
+
+	err := s.db.QueryRowContext(
+		ctx,
+		query,
+		user.Username,
+		user.Password,
+		user.Email,
+	).Scan(&user.ID, &user.CreatedAt)
+
+	if err != nil {
+		return err
+	}
+
 	return nil
+
 }
