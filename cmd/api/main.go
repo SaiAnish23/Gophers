0c997c4b9f7b4726a713db9be4257b1ad5590743
SaiAnish23 <anish231003@gmail.com>
2024-10-15 17:43:26 +0530

    feat(design):Repository Pattern implemented

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/SaiAnish23/Gophers/internal/env"
+	"github.com/SaiAnish23/Gophers/internal/store"
 	"github.com/joho/godotenv"
 )
 
@@ -17,10 +18,13 @@ func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":4000"),
 	}
+	store := store.NewStorage(nil)
 
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
+
 	mux := app.mount()
 	log.Fatal(app.run(mux))
 }
